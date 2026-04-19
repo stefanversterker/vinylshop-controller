b50bb41315e2f34a6e8363809e664672706bc10a
stefanversterker <stefanversterker@gmail.com>
2026-04-19 11:07:47 +0200

    project setup

---
+++ src/main/java/nl/novi/vinylshopcontroller/controllers/VinylshopControllerApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.vinylshopcontroller.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylshopControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinylshopControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/vinylshopcontroller/VinylshopControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.vinylshopcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylshopControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
